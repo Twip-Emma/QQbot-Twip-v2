7e74128a0381db682c432aa15ce5522dab4ebb0b
七画一只妖 <1157529280@qq.com>
2022-01-18 22:07:20 +0800

    修复bug

--- src/plugins/bean/sign_new/__init__.py
+++ src/plugins/bean/sign_new/__init__.py
@@ -2,12 +2,13 @@
 Author: 七画一只妖
 Date: 2022-01-18 21:03:02
 LastEditors: 七画一只妖
-LastEditTime: 2022-01-18 21:49:12
+LastEditTime: 2022-01-18 22:05:25
 Description: file content
 '''
 
 from nonebot import on_command
-from nonebot.adapters import Bot, Event
+# from nonebot.adapters import Bot, Event, GroupMessageEvent
+from nonebot.adapters.cqhttp import Bot, GroupMessageEvent
 from nonebot.typing import T_State
 from nonebot.rule import to_me
 
@@ -21,29 +22,26 @@ user_sign = on_command("签到", rule=to_me())
 
 
 # 求签
-# @get_power_in_sdorica
 @get_luck.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    user_id = str(event.get_user_id)
-    # group_id = str(event.get_group_id)
-    # group_id = str(event.get_session_id)
-    # print(group_id)
-    await get_luck.finish("hi")
-    # recall_user_info = await bot.get_group_member_info(group_id=group_id, user_id=user_id)
-    # recall_user_name = recall_user_info['nickname']
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
+    group_id = str(event.group_id)
+    user_id = str(event.user_id)
+    # await get_luck.finish("hi")
+    recall_user_info = await bot.get_group_member_info(group_id=group_id, user_id=user_id)
+    recall_user_name = recall_user_info['nickname']
 
-    # message = check_user_sign(user_id, recall_user_name)
+    message = check_user_sign(user_id, recall_user_name)
     # await session.send(message)
+    await get_luck.finish(message)
 
 
 # 签到（新版）
 @user_sign.handle()
-# @get_power_in_sdorica
-async def _(bot: Bot, event: Event):
-    # user_id = str(event.get_user_id)
-    # group_id = str(session.event.group_id)
-    # recall_user_info = await bot.get_group_member_info(group_id=group_id, user_id=user_id)
-    # user_name = recall_user_info['nickname']
-    # msg = sign_daily_start(user_name=user_name, user_id=user_id)
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
+    group_id = str(event.group_id)
+    user_id = str(event.user_id)
+    recall_user_info = await bot.get_group_member_info(group_id=group_id, user_id=user_id)
+    user_name = recall_user_info['nickname']
+    message = sign_daily_start(user_name=user_name, user_id=user_id)
     # await session.send(message=msg)
-    pass
+    await get_luck.finish(message)
