45cdb23fc98fccefe9ff1dfdccf2c55e1a9e7d75
七画一只妖 <1157529280@qq.com>
2022-08-21 23:40:16 +0800

    修复，闪照破解

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖
 Date: 2022-01-07 20:25:48
 LastEditors: 七画一只妖
-LastEditTime: 2022-08-11 17:47:09
+LastEditTime: 2022-08-21 23:28:28
 Description: file content
 '''
 import nonebot
@@ -25,11 +25,11 @@ nonebot.init(apscheduler_config={
 # nonebot.load_builtin_plugins()
 
 # 测试模块
-# nonebot.load_plugins("src/plugins/bean")
+nonebot.load_plugins("src/plugins/bean")
 
 # 正式模块
 # nonebot.load_plugins("src/plugins/admin")
-nonebot.load_plugins("src/plugins/function")
+# nonebot.load_plugins("src/plugins/function")
 # nonebot.load_plugins("src/plugins/user")
 # nonebot.load_plugins("src/plugins/listener")
 # nonebot.load_plugins("src/plugins/speaker")
