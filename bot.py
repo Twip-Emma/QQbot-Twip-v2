35fa55b8c2ec12a1c5902c9eac249f74facb6f9a
七画一只妖 <1157529280@qq.com>
2022-09-06 18:55:05 +0800

    群友抽卡、恢复：搜图

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖
 Date: 2022-01-07 20:25:48
 LastEditors: 七画一只妖
-LastEditTime: 2022-09-04 20:13:26
+LastEditTime: 2022-09-05 21:56:45
 Description: file content
 '''
 import nonebot
@@ -22,11 +22,11 @@ nonebot.init(apscheduler_config={
 })
 
 # 测试模块
-nonebot.load_plugins("src/plugins/bean")
+# nonebot.load_plugins("src/plugins/bean")
 
 # 正式模块
 # nonebot.load_plugins("src/plugins/admin")
-# nonebot.load_plugins("src/plugins/function")
+nonebot.load_plugins("src/plugins/function")
 # nonebot.load_plugins("src/plugins/user")
 # nonebot.load_plugins("src/plugins/listener")
 # nonebot.load_plugins("src/plugins/speaker")
