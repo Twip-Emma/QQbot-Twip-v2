02689ac7cf77fa7b6f7f322ae9075329b5fe37d8
七画一只妖 <1157529280@qq.com>
2022-09-17 14:29:44 +0800

    删除冗余资源

--- src/plugins/function/show_speak_rank/__init__.py
+++ src/plugins/function/show_speak_rank/__init__.py
@@ -2,21 +2,37 @@
 Author: 七画一只妖
 Date: 2022-03-01 20:27:45
 LastEditors: 七画一只妖
-LastEditTime: 2022-08-30 11:14:31
+LastEditTime: 2022-09-17 13:45:05
 Description: file content
 '''
 from nonebot import on_command
 from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
 from tool.find_power.format_data import is_level_A
+from nonebot.plugin import PluginMetadata
 
 from .db import *
 
 
+__plugin_meta__ = PluginMetadata(
+    name="查看水群排行",
+    description="查看所有群的水群前99名",
+    usage="查看水群排行"
+)
+
+
 SUPER = "1157529280"
 
-show_rank = on_command("查看水群排行", block=True, priority=2)
-show_rank_admin = on_command("查看水群排行-开发者模式", block=True, priority=2)
+show_rank = on_command("查看水群排行", block=True, priority=2, state={
+    'pm_usage': '查看水群排行',
+    'pm_describe': '查看所有群的水群前99名',
+    'pm_priority': 2
+})
+show_rank_admin = on_command("查看水群排行-开发者模式", block=True, priority=2, state={
+    'pm_usage': '**查看水群排行-开发者模式',
+    'pm_describe': '查看所有群的水群前99名，显示完整QQ号',
+    'pm_priority': 2
+})
 
 
 @show_rank.handle()
