caf8277dc588f79d5a60b52cbaaf60a1d26b9c0b
七画一只妖 <1157529280@qq.com>
2022-10-29 18:38:43 +0800

    修复SQL语句错误

--- Twip/__init__.py
+++ Twip/__init__.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2022-10-09 13:27:39
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2022-10-29 16:17:07
+LastEditTime: 2022-10-29 18:26:38
 '''
 from os import path
 from pathlib import Path
@@ -78,7 +78,7 @@ async def shutdown():
 # 加载自己写的模块
 # load_plugins("Twip/admin")
 # load_plugins("Twip/function")
-load_plugins("Twip/user")
+# load_plugins("Twip/user")
 load_plugins("Twip/listener")
 # load_plugins("Twip/speaker")
 # load_plugins("Twip/help")
