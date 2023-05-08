735a13cbb06acca388628693dc3255abccd0f078
七画一只妖 <1157529280@qq.com>
2023-05-08 16:33:16 +0800

    求签版本3初代4个结果

--- Twip/user/user_sign/payload/data/pray_txt.py
+++ Twip/user/user_sign/payload/data/pray_txt.py
@@ -2,64 +2,19 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2022-12-09 10:10:29
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2022-12-09 16:06:21
+LastEditTime: 2023-05-08 16:27:39
 '''
 PARY_TABLE = {
     "大吉": [
-        "The frost and wind have not been well-and thousands of birds are singing on the shore",
-        "There is nothing but dust",
-        "Leisure as spring cherry-and pure as autumn water",
-        "Time is like a circle-and luck will come again today",
-        "If they don t place their hopes on me-I will be my own sun",
-        "It takes no effort to get here without breaking through iron shoes",
-        "A wise man is more valuable than his time",
-        "Some next year-some good luck-just to hide",
-        "The sound of a magpie-the sound of a crow-the sound of a crow",
-        "If you want to know the cold food-listen to the magpie and hope to return",
-        "Gentlemen-Fang Gun-Good Luck in Tactics"
+        "皎洁"
     ],
     "中吉": [
-        "I just look down-but my heart is up",
-        "I m lucky-but not lucky",
-        "Look from afar-just meet and never forget",
-        "One flower opens-one touch smells",
-        "The most moving is refreshing-and people are intoxicated by flowers",
-        "The most brilliant bloom-the most beautiful accident",
-        "There are trees in the mountain and trees have branches-You are happy-but you don t know how much you cherish",
-        "The rain is the trace of the wind-the wind is the information of the rain-and the rainbow is the beauty after the rain",
-        "The reflection of the Weilan Lake is green-and I love Qianfan all my life",
-        "If you have a lover in your heart-you will see passers-by in your eyes",
-        "Just like the peach blossom and flowing water-the scenery along the way lingers curiously",
-        "The spring breeze walked idly by-and the peach blossoms smiled red on the side",
-        "Peach Blossom Face-Changing Seas"
+        "如月"
     ],
     "小吉": [
-        "In the end-it s all a joke",
-        "I m still waiting-always",
-        "Here we are-I wish you a bright future",
-        "Some people always say that you have changed-but no one asks what you have experienced.",
-        "I know there is no result-but I want to continue",
-        "Nobody can let me lose unless I don t want to win",
-        "Unique crows disdain white pigeons raised in captivity",
-        "I m like an outsider-smiling and shaking my head at my own story",
-        "Bless each other-one parting and two leniency",
-        "We have suffered a lot-If we don t grow up-we will be very poor",
-        "It s like the sunshine between your fingers-warm and beautiful-but you can never catch it",
-        "I didn t know each other at first-but I didn t know each other at last-just like a poem",
-        "If you are destined to be a passer-by why bother at first"
+        "摧残"
     ],
     "末吉": [
-        "Never remember-but never forget",
-        "Life is impermanent-life is fragile-and accidents and tomorrow are just night choices",
-        "Forget the past events and love and hatred",
-        "Don t sigh for the gloom of the human soul-how can we know that life and death are far apart",
-        "When you recite the mantra in your mouth-what you say is not what you think",
-        "My body is like a duckweed-I dare not say goodbye",
-        "We should be cautious in our words and actions",
-        "The destiny of heaven is like dew drops-like illusion-more like emptiness",
-        "Although there is something hard to say in my heart-I am lucky that someone asks me about it",
-        "Although everything is impermanent-it has its own natural and unrestrained meaning",
-        "Suddenly-I saw the willow color in the street and regretted teaching my husband to find a marquis",
-        "You can only miss the last bus until tomorrow-and"
+        "烈火"
     ],
 }
--- Twip/user/user_sign/payload/image_dao.py
+++ Twip/user/user_sign/payload/image_dao.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2022-12-09 09:17:23
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2023-04-30 19:06:16
+LastEditTime: 2023-05-08 14:58:30
 '''
 import datetime
 import random
@@ -19,21 +19,6 @@ from .image_factory import FontEntity, picture_paste_img, write_longsh
 BASE_PATH: str = Path(__file__).absolute().parents[0]
 
 
-# qq头像接口
-QQ_API1 = "http://q1.qlogo.cn/g?b=qq&nk=|QQ号码|&s=640"
-
-# qq群头像接口
-QQ_API2 = "http://p.qlogo.cn/gh/|QQ群号码|/|QQ群号码|/100/"
-
-
-# 获取头像
-def get_user_image(user_id: str) -> Image:
-    # 图片url转bytes
-    url = f"http://q1.qlogo.cn/g?b=qq&nk={user_id}&s=640"
-    res = requests.get(url)
-    byte_stream = BytesIO(res.content)
-    return Image.open(byte_stream)
-
 
 # 获取运势
 def get_lucky(user_id:str) -> str:
@@ -41,7 +26,7 @@ def get_lucky(user_id:str) -> str:
     if user == []: # 判断为新用户
         info = get_luck_info()
         insert_new_user(user_id, info)
-        return info
+        return info 
     else:
         now_time = datetime.datetime.now().strftime('%Y-%m-%d')
         if user[0][1] == now_time: # 今日已签到
@@ -53,115 +38,45 @@ def get_lucky(user_id:str) -> str:
 
 
 # 获取运势信息
-def get_luck_info() -> str:
+def get_luck_info():
     info = None
     luck_num = random.randint(1,1000)
-    print(luck_num)
 
-    flag = random.randint(1,100)
-    if flag <= 50:
-        if luck_num <= 30:
-            luck_num2 = random.randint(100000,10000000)
-            info = "大吉|" + random.choice(PARY_TABLE["大吉"]) + "|" + str(luck_num2) + "|繁     锦|A"
-        elif luck_num <= 100:
-            luck_num2 = random.randint(1000,99999)
-            info = "中吉|" + random.choice(PARY_TABLE["中吉"]) + "|" + str(luck_num2) + "|争     妍|A"
-        elif luck_num <= 300:
-            luck_num2 = random.randint(10,999)
-            info = "小吉|" + random.choice(PARY_TABLE["小吉"]) + "|" + str(luck_num2) + "|归     鸟|A"
-        elif luck_num <= 1000:
-            luck_num2 = random.randint(0,9)
-            info = "末吉|" + random.choice(PARY_TABLE["末吉"]) + "|" + str(luck_num2) + "|疏     影|A"
-        else:
-            luck_num2 = random.randint(100000,10000000)
-            info = "大吉|" + random.choice(PARY_TABLE["大吉"]) + "|" + str(luck_num2) + "|繁     锦|A"
-        return info
+    if luck_num <= 30:
+        luck_num2 = random.randint(100000,10000000)
+        info = random.choice(PARY_TABLE["大吉"])
+    elif luck_num <= 100:
+        luck_num2 = random.randint(1000,99999)
+        info = random.choice(PARY_TABLE["中吉"])
+    elif luck_num <= 300:
+        luck_num2 = random.randint(10,999)
+        info = random.choice(PARY_TABLE["小吉"])
+    elif luck_num <= 1000:
+        luck_num2 = random.randint(0,9)
+        info = random.choice(PARY_TABLE["末吉"])
     else:
-        if luck_num <= 30:
-            luck_num2 = random.randint(100000,10000000)
-            info = "大吉|" + random.choice(PARY_TABLE["大吉"]) + "|" + str(luck_num2) + "|渺 镜 云 烟|B"
-        elif luck_num <= 100:
-            luck_num2 = random.randint(1000,99999)
-            info = "中吉|" + random.choice(PARY_TABLE["中吉"]) + "|" + str(luck_num2) + "|纯 白 之 心|B"
-        elif luck_num <= 300:
-            luck_num2 = random.randint(10,999)
-            info = "小吉|" + random.choice(PARY_TABLE["小吉"]) + "|" + str(luck_num2) + "|诸 坠 渊 若|B"
-        elif luck_num <= 1000:
-            luck_num2 = random.randint(0,9)
-            info = "末吉|" + random.choice(PARY_TABLE["末吉"]) + "|" + str(luck_num2) + "|惄 焉 伤 悴|B"
-        else:
-            luck_num2 = random.randint(100000,10000000)
-            info = "大吉|" + random.choice(PARY_TABLE["大吉"]) + "|" + str(luck_num2) + "|渺 镜 云 烟|B"
-        return info
+        luck_num2 = random.randint(100000,10000000)
+        info = random.choice(PARY_TABLE["大吉"])
+    return f"{info}|{luck_num2}"
 
 
 # 制作背景总控
 def make_bg(user_id:str, user_name: str) -> Image:
-    user_head = get_user_image(user_id)
-    user_head = user_head.resize((300,300))
+    info = get_lucky(user_id).split("|")
 
 
-    info = get_lucky(user_id)
-    info = info.split("|")
+    # 写什么幸运度和昵称
+    ft = FontEntity(fsize=100, color="#00BFFF")
+    a2 = Image.open(Path(BASE_PATH)/r"image"/fr"{info[0]}.png")
+    a3 = write_longsh(ft, a2, info[0], "C", (1000,0))
+    ft.setColor("#00FFFF").setSize(35)
+    now_time = datetime.datetime.now().strftime('%Y-%m-%d')
+    a4 = write_longsh(ft, a3, f"幸运度：{info[1]}\n\n{user_name}   {now_time}", "C", (1150,0))
 
-    # 判断运势类型
-    if info[4] == "A":
-        bg = Image.open(Path(BASE_PATH)/r"image"/r"BaseBG.png")
-        location = int((bg.width - user_head.width)/2), 150
-        a1 = picture_paste_img(user_head, bg, location)
-        a2 = picture_paste_img(Image.open(Path(BASE_PATH)/r"image"/r"BaseBG3.png"), a1)
 
-        # 写什么运势
-        ft = FontEntity(fsize=100, color="#FF99FF")
-        a3 = write_longsh(ft, a2, info[3], "C", (450,0))
-        ft.setColor("#FF66FF").setSize(35)
-        now_time = datetime.datetime.now().strftime('%Y-%m-%d')
-        a4 = write_longsh(ft, a3, f"幸运度：{info[2]}\n\n{user_name}   {now_time}", "C", (600,0))
-        ft.setColor("#9933CC").setSize(25)
-        a5 = write_longsh(ft, a4, f"密语", "C", (730,0))
-
-        # 写密语
-        ft.setTTF(Path(BASE_PATH)/r"ttf"/r"Andrea.otf").setSize(30).setColor("#660099")
-        txt = info[1].replace("-","\n\n")
-        a6 = write_longsh(ft, a5, txt, "C", (780,0))
-
-        # 选择猫猫
-        luck = info[0]
-        luck = luck.replace(" ","")
-        neko = Image.open(Path(BASE_PATH)/r"image"/f"{luck}.png")
-        neko = neko.resize((int(neko.width * 0.5), int(neko.height * 0.5)))
-        a7 = picture_paste_img(neko, a6, (200, 600))
-        a8 = picture_paste_img(Image.open(Path(BASE_PATH)/r"image"/r"cover.png"), a7)
-
-        # 保存图片
-        save_path = str(Path(BASE_PATH)/r"cache"/f"{user_id}.jpg")
-        a8 = a8.convert("RGB")
-        a8.save(save_path)
-    else:
-        bg = Image.open(Path(BASE_PATH)/r"image"/r"BaseBG4.png")
-        location = int((bg.width - user_head.width)/2), 150
-        a1 = picture_paste_img(user_head, bg, location)
-        a2 = picture_paste_img(Image.open(Path(BASE_PATH)/r"image"/r"BaseBG5.png"), a1)
-        
-        # 写什么运势
-        ft = FontEntity(fsize=100, color="#0099FF")
-        a3 = write_longsh(ft, a2, info[3], "C", (480,0))
-        ft.setColor("#3399FF").setSize(35)
-        now_time = datetime.datetime.now().strftime('%Y-%m-%d')
-        a4 = write_longsh(ft, a3, f"幸运度：{info[2]}\n\n{user_name}   {now_time}", "C", (600,0))
-        ft.setColor("#0066CC").setSize(25)
-        a5 = write_longsh(ft, a4, f"密语", "C", (730,0))
-
-        # 写密语
-        ft.setTTF(Path(BASE_PATH)/r"ttf"/r"Andrea.otf").setSize(30).setColor("#3333FF")
-        txt = info[1].replace("-","\n\n")
-        a6 = write_longsh(ft, a5, txt, "C", (780,0))
-
-        a6 = picture_paste_img(Image.open(Path(BASE_PATH)/r"image"/r"cover2.png"), a6)
-
-        # 保存图片
-        save_path = str(Path(BASE_PATH)/r"cache"/f"{user_id}.jpg")
-        a6 = a6.convert("RGB")
-        a6.save(save_path)
+    # 保存图片
+    save_path = str(Path(BASE_PATH)/r"cache"/f"{user_id}.jpg")
+    a4 = a4.convert("RGB")
+    a4.save(save_path)
 
     return save_path
