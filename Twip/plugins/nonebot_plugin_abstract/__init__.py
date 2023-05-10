10a82c9d3a9ea8e1f0658528386936653f7b8321
七画一只妖 <1157529280@qq.com>
2023-05-10 10:39:42 +0800

    抽象功能-权限漏洞修复

--- Twip/plugins/nonebot_plugin_abstract/__init__.py
+++ Twip/plugins/nonebot_plugin_abstract/__init__.py
@@ -1,17 +1,27 @@
+'''
+Author: 七画一只妖 1157529280@qq.com
+Date: 2023-02-14 13:57:00
+LastEditors: 七画一只妖 1157529280@qq.com
+LastEditTime: 2023-05-10 10:39:17
+'''
 from nonebot import on_command
 from nonebot.log import logger
 from nonebot.typing import T_State
-from nonebot.adapters.onebot.v11 import Bot, Event, Message
+from nonebot.adapters.onebot.v11 import Bot, Event, Message, GroupMessageEvent
 from nonebot.params import CommandArg, ArgStr
 from .data import text_to_emoji
+from tool.find_power.format_data import is_level_S
 
+abstract = on_command("abstract", aliases={
+                      "抽象", "抽象化"}, priority=5, block=True)
 
-abstract = on_command("abstract", aliases={"抽象", "抽象化"}, priority=5, block=True)
 
 @abstract.handle()
-async def _(state: T_State, arg: Message = CommandArg()):
+@is_level_S
+async def _(state: T_State, event: GroupMessageEvent, cost=10, arg: Message = CommandArg()):
     if arg.extract_plain_text().strip():
         state["abstract"] = arg.extract_plain_text().strip()
+
 
 @abstract.got("abstract", prompt="你要抽象什么？")
 async def _(bot: Bot, event: Event, target_text: str = ArgStr("abstract")):
