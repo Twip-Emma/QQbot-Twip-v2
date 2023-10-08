fc63939d102c523e886c8f08b316ce0b51529aec
七画一只妖 <1157529280@qq.com>
2023-10-08 10:49:14 +0800

    坎公会战

--- Twip/function/kgqgj/payload2/api.py
+++ Twip/function/kgqgj/payload2/api.py
@@ -2,7 +2,7 @@
 Author: 七画一只妖 1157529280@qq.com
 Date: 2023-10-06 23:32:24
 LastEditors: 七画一只妖 1157529280@qq.com
-LastEditTime: 2023-10-08 10:44:52
+LastEditTime: 2023-10-08 10:47:45
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import httpx
@@ -14,9 +14,6 @@ import httpx
 
 # 全局变量下标
 req_index = 0
-
-# 记得填
-COOKIES = "i-wanna-go-back=-1; buvid_fp_plain=undefined; blackside_state=0; CURRENT_BLACKGAP=0; LIVE_BUVID=AUTO2916589792625841; DedeUserID=178547325; DedeUserID__ckMd5=d90ee02c8bad0cdd; buvid4=23F4DCF0-243F-05CD-0C32-458841A6D0A778370-022071818-OsDwyx78W%2ByrNhIDcatWKA%3D%3D; rpdid=|(m~Y|~~|~l0J'uYY)l~uu)l; is-2022-channel=1; hit-new-style-dyn=1; dy_spec_agreed=1; CURRENT_PID=0507da70-c954-11ed-b885-5544c605f9c2; FEED_LIVE_VERSION=V8; home_feed_column=5; buvid3=7E1CE2B0-09B1-38D0-C6CC-6F35C6A1603546445infoc; b_nut=1689678846; _uuid=A95CADCE-C543-3475-D712-102410C1ED869746942infoc; header_theme_version=CLOSE; nostalgia_conf=-1; hit-dyn-v2=1; Hm_lvt_7999812113dc28f5de372c8e8109efc4=1690549050; CURRENT_QUALITY=80; fingerprint=d3f0ed712f1dc1e49fa63fb6757c26e9; CURRENT_FNVAL=4048; browser_resolution=1528-742; SESSDATA=2552b3c6%2C1712120101%2C2c109%2Aa1CjBr1uCOGqfkU2SkCvF3i32CwXca8CBsgTSoCBTl5MuGyWIeSz8FV2bQLDJ27sNiIG0SVnNMQXQySU1lRzVTemNoSWdYcFBRalNzNVJGQU44Tk84RnR2UlotQjc4OHJ0QWNrRW93M2VCR3p4TDhNRHQxVUZxLW9VS1ltT0xUTEFSdDU5THcyRUdBIIEC; bili_jct=4c06c5ddbeed1bc26674fc0c7fe472dd; PVID=1; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTY4NTk5MjgsImlhdCI6MTY5NjYwMDY2OCwicGx0IjotMX0.Hh4tDG2AAd5GPvA07JXNqUeZ3pQkR6ssNs2JK6vEvVc; bili_ticket_expires=1696859868; bp_video_offset_178547325=849676824754520100; buvid_fp=d3f0ed712f1dc1e49fa63fb6757c26e9"
 
 SECRET_KEY = "192f4699782268107f37e8bd117812f"
 
